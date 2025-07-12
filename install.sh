93fbdb7825794e745436f1180ac424854d4bc328
shixun404 <shixun404@gmail.com>
2025-07-12 16:02:26 -0700

    update figures

--- install.sh
+++ install.sh
@@ -10,16 +10,16 @@ if [ -z "$PROJECT_ROOT" ]; then
 fi
 
 VARIANT_DIRS=(
-  "1D_A_exp_fft+cgemm+ifft"
-  "1D_B_exp_fused_fft_cgemm+ifft"
-  "1D_C_exp_fft+fused_cgemm_ifft"
-  "1D_D_exp_fused_fft_cgemm_ifft"
+  # "1D_A_exp_fft+cgemm+ifft"
+  # "1D_B_exp_fused_fft_cgemm+ifft"
+  # "1D_C_exp_fft+fused_cgemm_ifft"
+  # "1D_D_exp_fused_fft_cgemm_ifft"
   "1D_E_baseline"
-  "2D_A_exp_fft+cgemm+ifft"
-  "2D_B_exp_fused_fft_cgemm+ifft"
-  "2D_C_exp_fft+fused_cgemm_ifft"
-  "2D_D_exp_fused_fft_cgemm_ifft"
-  "2D_E_baseline"
+  # "2D_A_exp_fft+cgemm+ifft"
+  # "2D_B_exp_fused_fft_cgemm+ifft"
+  # "2D_C_exp_fft+fused_cgemm_ifft"
+  # "2D_D_exp_fused_fft_cgemm_ifft"
+  # "2D_E_baseline"
 )
 
 # === clinea ===
